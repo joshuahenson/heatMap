5fbb84831983ba4ca4ee9bd1e25b94eb0a135a90
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-05-18 19:40:49 -0700

    Added tooltip

--- js/index.js
+++ js/index.js
@@ -5,11 +5,19 @@ const margin = { top: 30, right: 20, bottom: 40, left: 60 };
 const width = 1200 - margin.left - margin.right;
 const height = 800 - margin.top - margin.bottom;
 
-const buildChart = (data) => {
+const tooltip = d3.select('#root').append('div')
+    .attr('class', 'tooltip')
+    .style('opacity', 0);
+
+// primary function to build chart
+const buildChart = (data, baseTemp) => {
+  const months = ['January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'];
+
   const colorScale = d3.scale.quantize()
     .domain(d3.extent(data, d => d.variance))
-    .range(['#2166ac', '#4393c3', '#92c5de', '#d1e5f0', '#f7f7f7',
-      '#fddbc7', '#f4a582', '#d6604d', '#b2182b']);
+    .range(['#053061', '#2166ac', '#4393c3', '#92c5de', '#d1e5f0', '#f7f7f7',
+      '#fddbc7', '#f4a582', '#d6604d', '#b2182b', '#67001f']);
 
   const monthScale = d3.time.scale()
     .domain([new Date(2015, 0, 1), new Date(2015, 11, 31)])
@@ -23,11 +31,11 @@ const buildChart = (data) => {
 
   const xScale = d3.scale.ordinal()
     .domain(d3.range(data.length / 12))
-    .rangeBands([0, width]);
+    .rangeBands([0, width], 0, 0);
 
   const yScale = d3.scale.ordinal()
     .domain(d3.range(12))
-    .rangeBands([0, height], 0.01, 0);
+    .rangeBands([0, height], 0, 0);
 
   const xAxis = d3.svg.axis()
     .scale(yearScale)
@@ -41,23 +49,36 @@ const buildChart = (data) => {
     .orient('left');
 
   const svg = d3.select('#root')
-  .append('svg')
-    .attr('class', 'chart')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-  .append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+    .append('svg')
+      .attr('class', 'chart')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
   // append colored temp rects
   svg.selectAll('rect')
     .data(data)
     .enter().append('rect')
-    .attr('id', (d, i) => i)
     .attr('x', (d, i) => xScale(Math.floor(i / 12)))
     .attr('y', (d, i) => yScale(i % 12))
     .attr('width', xScale.rangeBand())
     .attr('height', yScale.rangeBand())
-    .style('fill', d => colorScale(d.variance));
+    .style('fill', d => colorScale(d.variance))
+    .style('stroke', 'gray')
+    .style('stroke-width', 0.1)
+    .on('mouseover', d => {
+      tooltip.transition()
+        .style('opacity', 1);
+      tooltip.html(`<h3>${months[d.month - 1]} ${d.year}</h3>
+        ${(baseTemp + d.variance).toFixed(2)}&deg;C`)
+          .style('left', `${d3.event.pageX + 20}px`)
+          .style('top', `${d3.event.pageY - 30}px`);
+    })
+    .on('mouseout', () => {
+      tooltip.transition()
+        .style('opacity', 0);
+    });
 
   // append x axis & label
   svg.append('g')
@@ -93,5 +114,5 @@ d3.json('../temp/global-temperature.json', (error, data) => {
   //   console.log(error);
   // }
   // console.log(data);
-  buildChart(data.monthlyVariance);
+  buildChart(data.monthlyVariance, data.baseTemperature);
 });
